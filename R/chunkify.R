a59398de400ad4a9a28b1b6678e84bdf1274ddb1
Jonathan Sidi <yonicd@gmail.com>
2017-11-10 16:33:44 -0500

    add chunky_opts

--- R/chunkify.R
+++ R/chunkify.R
@@ -23,27 +23,25 @@ chunkify <- function() {
   adc$selection[[1]]$range$end[[1]] <- newend
   adc$selection[[1]]$range$end[[2]] <- 1
   
-  # Insert text that splits the code chunk in two
-  rstudioapi::insertText(location =  adc$selection[[1]]$range$start,"\n```{r}\n")
-  rstudioapi::insertText(location =  adc$selection[[1]]$range$end,"```\n")
-
-}
-
-#' @export
-chunkify_time <- function(date=Sys.time()) {
+  start_text <- "\n```{r"
   
-  # Get document context
-  # to get cursor position
-  adc <- rstudioapi::getActiveDocumentContext()
+  if(!chunky_opts$get('basic')){
+    if(chunky_opts$get('counter')){
+      chunky_num <- length(grep(sprintf('\\`\\`\\`\\{r %s',chunky_opts$get('name')),adc$contents[1:adc$selection[[1]]$range$start[[1]]])) + 1
+      start_text <- paste(start_text,sprintf('%s%03d',chunky_opts$get('name'),chunky_num))      
+    }else{
+      start_text <- paste(start_text,chunky_opts$get('name'))
+    }
+      
   
-  newend <- adc$selection[[1]]$range$start[[1]]+(adc$selection[[1]]$range$end[[1]]-adc$selection[[1]]$range$start[[1]])+3
   
-  adc$selection[[1]]$range$start[[1]] <- adc$selection[[1]]$range$start[[1]]-1
-  adc$selection[[1]]$range$end[[1]] <- newend
-  adc$selection[[1]]$range$end[[2]] <- 1
+  if(!is.null(chunky_opts$get('chunk_opts')))
+    start_text <- paste(start_text,chunky_opts$get('chunk_opts'),sep=',')}
+  
+  start_text <- paste0(start_text,'}\n')
   
   # Insert text that splits the code chunk in two
-  rstudioapi::insertText(location =  adc$selection[[1]]$range$start,sprintf("\n```{r %s}\n",date))
-  rstudioapi::insertText(location =  adc$selection[[1]]$range$end,"```\n")
+  rstudioapi::insertText(location =  adc$selection[[1]]$range$start,start_text)
+  rstudioapi::insertText(location =  adc$selection[[1]]$range$end,"\n```\n")
 
 }
